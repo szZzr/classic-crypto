11ecd560f8104c3d9046c987b3840b713d03b486
rzs <sizorcsd@hotmail.gr>
2022-08-03 12:26:03 +0300

    Platon fixed and optimized.

---
+++ README.md
@@ -0,0 +1,22 @@
+At this repository presents classic cryptographic exercises, the solutions and the way to solve them, in the context of the  postgraduate cryptography course as part of the MsC education program Communication Networks and Systems Security at the Informatics department of AUTH, taught by Asssistant Professor K. A. Draziotis. In the context of solving the exercises, code was developed in Python, the copyrights of them belong exclusively to the owner of this repository. In detail, the contents of interest evolve methods and algorithms of symmetric and asymmetric cryptography and the table of contents presented below.
+
+1. Shift Cipher
+2. Vigenere
+3. Cyclic Shift Cipher
+4. One Time Pad
+5. RC4
+6. S-Boxes
+7. Avalanche Effect
+8. Diffie-Helman Key Exchange
+9. Karatsuba (fast exponential algorithm)
+10. Platon's Number
+11. Prime Test
+12. Safe Primes (2048bits, <100sec)
+13. RSA
+14. Wienner Attack
+15. More Symmetric Algorithms
+    1. Blowfish
+    2. Entropy
+    3. LFSR
+
+Στο αποθετήρειο αυτό παρουσιάζονται οι εκφωνήσεις και οι τρόποι επίλυσης κλασσικών κρυπτογραφικών ασκήσεων στα πλαίσια του μεταπτυχιακού μαθήματος κρυπτογραφίας, μέρος του προγράμματος των μεταπτυχιακών σπουδών Δίκτυα Επικοινωνιών και Ασφάλεια Συστημάτων του τμήματος Πληροφορικής του ΑΠΘ, με διδάσκοντα τον Κ. Δραζιώτη. Στο πλαίσιο της επίλυσης των ασκήσεων αναπτύχθηκε κώδικας σε Python τα δικαιώματα του οποίου ανήκουν αποκλειστηκά και μόνο στον κάτοχο του εν λόγω αποθετηρίου. Αναλυτικότερα τα περιεχόμενα του αφορούν τις παρακάτω περίπτωσεις μεθόδων και αλγορίθμων της συμμετρικής και ασύμμετρης κρυπτογραφίας.
--- src/10-Platon/Platon-exercise.py
+++ src/10-Platon/Platon-exercise.py
@@ -1,7 +1,8 @@
 import sys
 sys.path.append('..')
 from utilities import pow, karatsuba, profiler
-from math import log10 as log, e as epsilon
+from miller_rabin import miller_rabin
+from math import log, e as epsilon, ceil as upper_bound
 gamma = 0.577
 egamma = epsilon ** gamma
 
@@ -21,24 +22,37 @@ def sum_divisors_slow(n:int):
     return sum
 
 
-def sum_divisors(n:int):
+def sum_divisors_old(n: int):
     sum = 0
-    for i in range(1, n+1):
-        if n%i==0:
-            sum+=i
+    max_divisor = (n + 1) // 2
+    for i in range(n+1, 1,-1):
+        if n % i == 0:
+            sum += i
+    return sum
+
+def sum_divisors(n: int, limit:int):
+    sum = 0
+    max_divisor = (n + 1) // 2
+    for i in range(max_divisor+1, n//2,-1):
+        if n % i == 0:
+            sum += i
+    if sum>= limit//2:
+        for i in range(n//2, 1,-1):
+            if n % i == 0:
+                sum += i
     return sum
 
 
 def feature(n:int)->float:
     # e_g = pow(epsilon,gamma)
-    ln = log(log(n))
+    ln = log(log(n)) # by default base-e
     return egamma*n*ln
 
 
 def solve():
-    s5040 = sum_divisors(5040)
+    s5040 = sum_divisors_old(5040)
     for i in range(5030, 10**7 ): # pow(10,7)
-        s = sum_divisors(i)
+        s = sum_divisors_old(i)
         # s = len(divisors(i))
         feat = feature(i)
         # if s> s5040:
@@ -47,6 +61,37 @@ def solve():
             print(f'{i} -> sum: {s} _ condition: {feat}')
             # print(s)
 
+def test(i:int):
+    feat = feature(i)
+    # s = sum_divisors(i,upper_bound(feat))
+    s = sum_divisors_old(i)
+    if s > feat:
+        print(f'{i} -> sum: {s} _ condition: {feat}')
+        return True
+    return False
 
 
-solve()
+def solve_parallel():
+    import os
+    from concurrent.futures import ProcessPoolExecutor
+
+    min = 5030
+    max = 10**7
+    workers = os.cpu_count()
+
+    print(f"\nRun parallel\nnumber-of-workers: {workers}\ntest-range: [{min},{max}]\n")
+    executor = ProcessPoolExecutor(max_workers=workers)
+    results = executor.map(test, list(range(min, max )))
+
+    proves = sum(list(results))
+    if proves==1:
+        print('Platon\'s statement is True.')
+    else:
+        print(f'Platon has failed !!!\nFind total: {proves}')
+
+
+solve_parallel()
+
+# import cProfile
+# cProfile.run('solve_parallel()')
+# cProfile.run('solve()')
